c529c2da65d7a96e9dd2fad12ce363111cc2c44b
NicolasRodriguezH <nicolasrhila@gmail.com>
2023-02-07 13:13:17 -0500

    Tests for set and get attributes of a model

---
+++ tests/Unit/Models/PostTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Post;
+use PHPUnit\Framework\TestCase;
+
+class PostTest extends TestCase
+{
+    public function test_set_name_in_lowercase()
+    {
+        $post = New Post;
+        $post->name = 'Proyecto Laravel PHP';
+
+        $this->assertEquals('proyecto laravel php', $post->name);
+    }
+
+    public function test_get_slug()
+    {
+        $post = New Post;
+        $post->name = 'Proyecto Laravel PHP';
+
+        $this->assertEquals('proyecto-laravel-php', $post->slug);
+    }
+}
